5a5986d6c6d24a7bc19daf0848843f0a6e88e526
Илья Тищенко <ilya.tishencko676@yandex.ru>
2021-01-11 11:44:10 +0300

    errorMiddleware and productRoutes

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,25 @@
+/* Includes */
+
+// import node_modules
+
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
+
+// import mongoDB connection config
+
 import connectDB from './config/db.js'
+
+// import routes
+
+import productRoutes from './routes/productRoutes.js'
+
+// import middleware functions
+
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+
+
+
 
 const PORT = process.env.PORT || 5000
 
@@ -14,5 +32,16 @@ const app = express()
 app.get('/', (req, res) => {
   res.send('API is running...')
 })
+
+
+// Routes
+
+app.use('/api/products', productRoutes)
+
+
+// Error middleware functions
+
+app.use(notFound)
+app.use(errorHandler)
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
